1373fbb8838d88739fc8500566c6d78f5ab3fb73
Ariefyusufwinata <winata.arief26@gmail.com>
2024-06-24 08:51:10 +0700

    update helpers

--- app/Helpers/ApiHelpers.php
+++ app/Helpers/ApiHelpers.php
@@ -9,7 +9,10 @@ class ApiHelpers
         'message' => null,
     ];
 
-    public static function success($data = null, $message = null, $code = 200)
+    /**
+     * When the request is successful
+     */
+    public static function ok($data = null, $message = null, $code = 200)
     {
         self::$response['code'] = $code;
         self::$response['status'] = 'success';
@@ -19,7 +22,49 @@ class ApiHelpers
         return response()->json(self::$response, self::$response['code']);
     }
 
-    public static function error($error = null, $message = null, $code = 400)
+    /**
+     * When the request is successful added data
+     */
+    public static function success($data = null, $message = null, $code = 201)
+    {
+        self::$response['code'] = $code;
+        self::$response['status'] = 'success';
+        self::$response['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['code']);
+    }
+
+    /**
+     * Whe the request is failed or error
+     */
+    public static function badRequest($error = null, $message = null, $code = 400)
+    {
+        self::$response['code'] = $code;
+        self::$response['status'] = 'error';
+        self::$response['message'] = $message;
+        self::$response['error'] = $error;
+
+        return response()->json(self::$response, self::$response['code']);
+    }
+
+    /**
+     * Whe the request is already exist
+     */
+    public static function alredy($error = null, $message = null, $code = 401)
+    {
+        self::$response['code'] = $code;
+        self::$response['status'] = 'error';
+        self::$response['message'] = $message;
+        self::$response['error'] = $error;
+
+        return response()->json(self::$response, self::$response['code']);
+    }
+
+        /**
+     * Whe the request is Internal Server Error
+     */
+    public static function internalServer($error = null, $message = null, $code = 500)
     {
         self::$response['code'] = $code;
         self::$response['status'] = 'error';
